577f723592a7c6033be696ad9dc770409334d352
plloyd11 <plloyd11@gmail.com>
2020-01-04 10:20:55 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'primary-color': '#ff9f39',
+        'primary-color-dark': '#FF8605',
+        'dark-text': '#563715'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
